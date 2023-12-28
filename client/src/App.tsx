e625f20d255b46ca83e9da76765a11b0b5cf6ad7
skrupulezno <skrupulezno@gmail.com>
2023-12-28 21:39:50 +0900

    added notes 0.1

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Todolist from './Todolist';
+import {Todolist, TaskType} from './Todolist';
 
 function App() {
-  return (
+  let tasks1: Array<TaskType> = [
+    {id:1, title: "Python", isDone: true},
+    {id:2, title: "JavaScript", isDone: false},
+    {id:3, title: "C++", isDone: true},
+    {id:4, title: "C#", isDone: true},
+    {id:5, title: "C", isDone: true},
+    {id:6, title: "TypeScript", isDone: false}
+  ]
+
+  let tasks2: Array<TaskType> = [
+    /*{id:1, title: "Dota2", isDone: true},
+    {id:2, title: "CS2", isDone: false},
+    {id:3, title: "Valorant", isDone: false},
+    {id:3, title: "Overwatch", isDone: false}*/
+  ]
+
+  function removeTask(id: number) {
+    let resultTasks = tasks1.filter( () => {return true} );
+    console.log(resultTasks);
+  }
+
+  
+ return (
     <div className="App">
-      <Todolist/>
-      <Todolist/>
-      <Todolist/>
+        <Todolist title="zxc"
+        tasks={tasks1}
+        removeTask={removeTask}/>
+        <Todolist title="name" 
+        tasks={tasks2} 
+        removeTask={removeTask}/>
     </div>
   );
 }
--- client/src/Todolist.tsx
+++ client/src/Todolist.tsx
@@ -1,19 +1,34 @@
 import React from 'react';
-import './App.css';
 
-function Todolist() {
+export type TaskType = {
+    id: number
+    title: string
+    isDone: boolean
+}
+
+type PropsType = {
+    title: string
+    tasks: Array<TaskType>
+    removeTask: Function
+}
+
+export function Todolist(props: PropsType, ) {
     return (
         <div>
-            <h3>What to learn</h3>
+            <h3>{props.title}</h3>
             <p>я дурак ты дурак</p>
             <div>
                 <input/>
-                <button>add</button>
+                <button>add</button>  
             </div>
             <ul>
-                <li><input type="checkbox" checked={false}></input><span>cs2</span></li>
-                <li><input type="checkbox" checked={true}></input><span>csgo</span></li>
-                <li><input type="checkbox" checked={true}></input><span>cs1.6</span></li>
+                {
+                    props.tasks.map( (t) => {
+                        return <li><input type="checkbox" checked={t.isDone}/>
+                        <span>{t.title}</span>
+                        <button onClick={ () => {props.removeTask(t.id)} }>x</button></li>
+                    })
+                }
             </ul>
             <div>
                 <button>All</button>
@@ -23,5 +38,3 @@ function Todolist() {
         </div>
     )
 }
-
-export default Todolist;
